56e739046b05a6bb7369d5c757e701a8f956a5cd
L1nMay <ilazayzev@gmail.com>
2025-04-25 03:35:10 -0400

    New project

--- test/AccountTest.cpp
+++ test/AccountTest.cpp
@@ -1,47 +1,26 @@
-#include "gtest/gtest.h"
-#include "gmock/gmock.h"
+#include "gtest/gtest.h" // Changed include
 #include "Account.h"
 
-#include <stdexcept>
-
-TEST(AccountTest, ConstructorValidInitialBalance) {
-    Account account("12345", 100.0);
-    ASSERT_EQ(100.0, account.getBalance());
-    ASSERT_EQ("12345", account.getAccountNumber());
+TEST(AccountTest, InitialBalanceIsZero) {
+    Account account;
+    ASSERT_EQ(account.getBalance(), 0);
 }
 
-TEST(AccountTest, ConstructorInvalidInitialBalance) {
-    ASSERT_THROW(Account("12345", -100.0), std::invalid_argument);
+TEST(AccountTest, DepositIncreasesBalance) {
+    Account account;
+    account.deposit(100);
+    ASSERT_EQ(account.getBalance(), 100);
 }
 
-TEST(AccountTest, DepositValidAmount) {
-    Account account("12345", 100.0);
-    account.deposit(50.0);
-    ASSERT_EQ(150.0, account.getBalance());
+TEST(AccountTest, WithdrawDecreasesBalance) {
+    Account account;
+    account.deposit(100);
+    account.withdraw(50);
+    ASSERT_EQ(account.getBalance(), 50);
 }
 
-TEST(AccountTest, DepositInvalidAmount) {
-    Account account("12345", 100.0);
-    ASSERT_THROW(account.deposit(-50.0), std::invalid_argument);
-    ASSERT_THROW(account.deposit(0.0), std::invalid_argument);
-}
-
-TEST(AccountTest, WithdrawSufficientFunds) {
-    Account account("12345", 100.0);
-    bool success = account.withdraw(50.0);
-    ASSERT_TRUE(success);
-    ASSERT_EQ(50.0, account.getBalance());
-}
-
-TEST(AccountTest, WithdrawInsufficientFunds) {
-    Account account("12345", 100.0);
-    bool success = account.withdraw(150.0);
-    ASSERT_FALSE(success);
-    ASSERT_EQ(100.0, account.getBalance());
-}
-
-TEST(AccountTest, WithdrawInvalidAmount) {
-    Account account("12345", 100.0);
-    ASSERT_THROW(account.withdraw(-50.0), std::invalid_argument);
-    ASSERT_THROW(account.withdraw(0.0), std::invalid_argument);
+TEST(AccountTest, WithdrawMoreThanBalanceFails) {
+    Account account;
+    ASSERT_FALSE(account.withdraw(100));
+    ASSERT_EQ(account.getBalance(), 0);
 }
